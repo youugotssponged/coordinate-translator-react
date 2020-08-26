f7cc6b16f9593a630952e6fbbd8b294767aedba7
youugotssponged <jordanmccann64@outlook.com>
2020-08-26 03:32:40 +0100

    (fix): Fixed webpack issue with images, added image to header

--- src/App.js
+++ src/App.js
@@ -11,16 +11,34 @@ import "./Components/layout.css";
 import colors from "./Common/colors";
 import fonts from "./Common/fonts";
 
-const TestStyledDiv = styled.div`
+// Simple test using styled div to create the header of the page.
+const HeaderDiv = styled.div`
+    display: flex;
+    justify-content: center;
     background-color: ${colors.darkBlue};
     color: white;
     width: 100%;
     height: 100px;
+    z-index: -1;
 
     & h1 {
         font-family: ${fonts.titleFont};
         font-size: 90px;
         text-align: center;
+        overflow: hidden;
+        margin-bottom: 20px;
+        z-index: 1;
+    }
+
+    & img {
+        position: absolute;
+        width: 200px;
+        height: 200px;
+        right: 50px;
+        top: -50px;
+        clip: rect(0px, 200px, 150px, 0px);
+        z-index: 0;
+        filter: invert(1);
     }
 `;
 
@@ -33,9 +51,13 @@ const App = () => {
                     rel="stylesheet"
                 />
             </Helmet>
-            <TestStyledDiv>
+            <HeaderDiv>
                 <h1> Coordinate To OSGB Converter</h1>
-            </TestStyledDiv>
+                <img
+                    src="/img/os_grid_icon.png"
+                    alt="OS GRID logo from FunkyPenguin10's Flutter App"
+                ></img>
+            </HeaderDiv>
         </>
     );
 };
