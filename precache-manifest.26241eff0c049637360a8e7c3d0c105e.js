f796a04e2166a0efd91bcb16718038c92130fdce
youugotssponged <jordanmccann64@outlook.com>
2020-09-02 21:46:12 +0100

    Updates

---
+++ precache-manifest.26241eff0c049637360a8e7c3d0c105e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d1217bb4d737196255f0e85c3c3049",
+    "url": "/coordinate-translator-react/index.html"
+  },
+  {
+    "revision": "afba6323a354e519cbc8",
+    "url": "/coordinate-translator-react/static/css/main.7cbd8309.chunk.css"
+  },
+  {
+    "revision": "489dc9ad7cfddc8e4304",
+    "url": "/coordinate-translator-react/static/js/2.b0af891e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coordinate-translator-react/static/js/2.b0af891e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afba6323a354e519cbc8",
+    "url": "/coordinate-translator-react/static/js/main.54eaf322.chunk.js"
+  },
+  {
+    "revision": "2214517e0992801e77c7",
+    "url": "/coordinate-translator-react/static/js/runtime-main.dd2b129b.js"
+  }
+]);
